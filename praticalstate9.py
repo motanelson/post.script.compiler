e1e39bbe0c229c50369c0302a3f56c683209b179
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-10 13:43:31 +0100

    Add files via upload

---
+++ praticalstate9.py
@@ -0,0 +1,20 @@
+class state9:
+    def __init__(self,live):
+        self.live=int(float(float(live)/11.1112))
+        if live>9:
+            self.live=9
+    
+    def lost(self):
+            self.live=self.live-1
+            if self.live<1:
+               self.live=0
+
+
+    def report(self):
+        print("live : "+str(int(self.live * 11.1112))+" % ")
+
+
+a=state9(100)
+for b in range(9):
+    a.report()
+    a.lost()
